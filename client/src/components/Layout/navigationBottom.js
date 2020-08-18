1f22c32eccbd8317a71ff05469c97166c1ee168d
ludocourbin <ludovic.courbin@gmail.com>
2020-08-18 16:01:43 +0200

    pre merge dev

--- client/src/components/Layout/navigationBottom.js
+++ client/src/components/Layout/navigationBottom.js
@@ -3,25 +3,23 @@ import { Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
 const NavigationBottom = ({ user }) => {
-    return (
-        <div className="navigationBottom">
-            <NavLink to={user.slug}>
-                <Icon
-                    className="header-icon navigationBottom-items"
-                    name="user"
-                    size="big"
-                />
-            </NavLink>
-
-            <NavLink to="/">
-                <Icon
-                    className="header-icon navigationBottom-items"
-                    name="home"
-                    size="big"
-                />
-            </NavLink>
-        </div>
-    );
+    return <div className="navigationBottom"></div>;
 };
 
 export default NavigationBottom;
+
+// <NavLink to={user.slug}>
+//                 <Icon
+//                     className="header-icon navigationBottom-items"
+//                     name="user"
+//                     size="big"
+//                 />
+//             </NavLink>
+
+//             <NavLink to="/">
+//                 <Icon
+//                     className="header-icon navigationBottom-items"
+//                     name="home"
+//                     size="big"
+//                 />
+//             </NavLink>

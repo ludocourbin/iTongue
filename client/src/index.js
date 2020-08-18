ebb492ce18475d02ad095ef162bf2027596a508a
ludocourbin <ludovic.courbin@gmail.com>
2020-08-18 16:34:11 +0200

    fixed react strict mode

--- client/src/index.js
+++ client/src/index.js
@@ -10,15 +10,13 @@ import { store, persistor } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <Router>
-                    <App />
-                </Router>
-            </PersistGate>
-        </Provider>
-    </React.StrictMode>,
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <Router>
+                <App />
+            </Router>
+        </PersistGate>
+    </Provider>,
     document.getElementById("root")
 );
 

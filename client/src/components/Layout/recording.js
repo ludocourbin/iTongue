44398d5f54962c3bd611a122280a022398697c2b
ludocourbin <ludovic.courbin@gmail.com>
2020-08-19 19:50:14 +0200

    handle isRecording  and selected audio to record

---
+++ client/src/components/Layout/recording.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import './Recording.scss';
+
+const Recording = () => {
+    return (
+        <div className="recording">
+            <h1 className="">hello</h1>
+        </div>
+    );
+};
+
+export default Recording;

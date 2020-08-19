41e69d68891ad5de28e01e7a4932282d8f654d15
Quentin <q.lemogodeuc@gmail.com>
2020-08-19 15:12:37 +0200

    docs(routes): add comments needed to generate swagger UI

--- server/app/routes/language-routes.js
+++ server/app/routes/language-routes.js
@@ -4,6 +4,44 @@ const languageController = require("../controllers/language-controller");
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /languages:
+ *  get:
+ *      tags: 
+ *        - Languages
+ *      summary: Get all languages
+ *      description: This route serves all languages - Array of objects
+ *      responses:
+ *          '200':
+ *              description: OK
+ *              content:
+ *                application/json:
+ *                  schema:
+ *                    type: array
+ *                    items:
+ *                      type: object
+ *                      properties:
+ *                        id:
+ *                          type: integer
+ *                        name:
+ *                           type: string
+ *                        code:
+ *                           type: string
+ *                    example:
+ *                       datas: [ 
+ *                          { 
+ *                            id: 1,
+ *                            name: french,
+ *                            code: fr
+ *                          },
+ *                          { 
+ *                            id: 2,
+ *                            name: spanish,
+ *                            code: es_ES
+ *                          },
+ *                       ]
+ */
 router.get("/", languageController.getAll);
 
 module.exports = router;

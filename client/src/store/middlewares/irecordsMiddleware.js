e6512989d574fc3b8fca64cba0ee2f5e70f5f89d
ludocourbin <ludovic.courbin@gmail.com>
2020-08-20 16:24:08 +0200

    send records to server

--- client/src/store/middlewares/irecordsMiddleware.js
+++ client/src/store/middlewares/irecordsMiddleware.js
@@ -13,29 +13,29 @@ export default (store) => (next) => (action) => {
             const blob = action.payload;
             const user = store.getState().user.currentUser;
             const translation = { id: 11 };
-
             const file = new File([blob], "iRecord");
-            console.log(file);
-        // const data = new FormData();
-        // data.append("translation_id", translation.id);
-        // data.append("record", blob);
 
-        // const config = {
-        //     header: { "Content-Type": "multipart/form-data" },
-        // };
+            const data = new FormData();
+            data.append("translation_id", translation.id);
+            // data.append("record", blob);
+            data.append("record", file);
 
-        // axios
-        //     .post(
-        //         `https://itongue.herokuapp.com/users/${user.id}/record`,
-        //         data,
-        //         config
-        //     )
-        //     .then((res) => {
-        //         store.dispatch(sendIrecordsSuccess(res));
-        //     })
-        //     .catch((err) => {
-        //         store.dispatch(sendIrecordsError(err));
-        //     });
+            const config = {
+                header: { "Content-Type": "multipart/form-data" },
+            };
+
+            axios
+                .post(
+                    `https://itongue.herokuapp.com/users/${user.id}/record`,
+                    data,
+                    config
+                )
+                .then((res) => {
+                    store.dispatch(sendIrecordsSuccess(res));
+                })
+                .catch((err) => {
+                    store.dispatch(sendIrecordsError(err));
+                });
 
         default:
             return;

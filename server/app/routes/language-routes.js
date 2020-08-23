491960e82fc5799a3f9a4415de75113c30b6946a
Quentin <q.lemogodeuc@gmail.com>
2020-08-23 18:30:30 +0200

    fix(route): add route to query language by id

--- server/app/routes/language-routes.js
+++ server/app/routes/language-routes.js
@@ -44,4 +44,41 @@ const router = express.Router();
  */
 router.get("/", languageController.getAll);
 
+/**
+ * @swagger
+ * /languages/{id}:
+ *  get:
+ *      tags: 
+ *        - Languages
+ *      summary: Get one language
+ *      description: This route serves one language
+ *      parameters:
+ *        - name: id
+ *          description: id to get language by
+ *          in: path
+ *          type: integer
+ *          required: true
+ *      responses:
+ *          '200':
+ *              description: OK
+ *              content:
+ *                application/json:
+ *                  schema:
+ *                    type: object
+ *                    properties:
+ *                      id:
+ *                        type: integer
+ *                      name:
+ *                         type: string
+ *                      code:
+ *                         type: string
+ *                    example:
+ *                       datas: { 
+ *                            id: 1,
+ *                            name: french,
+ *                            code: fr
+ *                          }
+ */
+router.get("/:id([0-9]+)", languageController.getOne);
+
 module.exports = router;

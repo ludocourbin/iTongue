2afa02773cc5459eb077c73653520f4f8b4fa821
Quentin <q.lemogodeuc@gmail.com>
2020-08-27 12:24:50 +0200

    build(config): add node-fetch

--- server/app/middlewares/recaptcha-middleware.js
+++ server/app/middlewares/recaptcha-middleware.js
@@ -4,26 +4,18 @@ module.exports = async (req, res, next) => {
   const { captcha } = req.body;
   const secretKey = process.env.RECAPTCHA_SECRET;
   const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${captcha}`;
-  
-  console.log("...Verifyning reCaptcha");
-  console.table([{ publicKey: captcha }], ["publicKey"]);
-  console.table([{ secretKey }], ["secretKey"]);
-  
+
   if (!captcha) {
-    return res.status(400).json({ errors: { msg: "Clé publique reCaptcha manquante ou invalide" } });
+    return res.status(400).json({ errors: { msg: "Le token reCaptcha est manquant ou invalide" } });
   }
 
   try {
     const verified = await fetch(verifyUrl, { method: "POST" });
     const response = await verified.json();
 
-    console.table([response]);
-    // Score déterminé arbitrairement, à discuter
-    if(!response.success || response.score < .3) return res.status(429).json(response);
+    if (!response.success || response.score < 0.3) return res.status(429).json(response);
 
-    res.json(response);
-    // next();
-
+    next();
   } catch (error) {
     next(error);
   }

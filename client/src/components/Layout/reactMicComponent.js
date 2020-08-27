85d40c32cef681a95566aad6d4f3169f872b84b9
ludocourbin <ludovic.courbin@gmail.com>
2020-08-27 12:36:13 +0200

    updated to record audio without library

--- client/src/components/Layout/reactMicComponent.js
+++ client/src/components/Layout/reactMicComponent.js
@@ -1,52 +1,68 @@
 import React, { useState } from "react";
-import { ReactMic } from "react-mic";
+// import { ReactMic } from "react-mic";
 import { Icon } from "semantic-ui-react";
 import AudioPlayer from "../../containers/Audio";
+import AudioRecorder from "audio-recorder-polyfill";
+import mpegEncoder from "audio-recorder-polyfill/mpeg-encoder";
+
+AudioRecorder.encoder = mpegEncoder;
+AudioRecorder.prototype.mimeType = "audio/mpeg";
+window.MediaRecorder = AudioRecorder;
+
 // import './ReactMicComponent.scss';
 
 const ReactMicComponent = ({ setRecordedSound, recordedSound }) => {
     const [micLoading, setMicLoading] = useState(true);
     const [recording, setRecording] = useState(false);
+    const [recorder, setRecorder] = useState(null);
 
-    const onStop = (recordedBlob) => {
-        console.log("recordedBlob is: ", recordedBlob);
-        setRecordedSound(recordedBlob);
-    };
+    // const onStop = (recordedBlob) => {
+    //     console.log("recordedBlob is: ", recordedBlob);
+    //     setRecordedSound(recordedBlob);
+    // };
 
-    const startRecording = () => {
+    const startRecording = async () => {
+        console.log("about to record some sound");
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        const rcd = new MediaRecorder(stream);
+        rcd.addEventListener("dataavailable", (e) => {
+            setRecordedSound(URL.createObjectURL(e.data));
+        });
+
+        rcd.start();
         setRecording(true);
-        setTimeout(() => {
-            setMicLoading(false);
-        }, 1000);
+
+        setRecorder(rcd);
     };
+
+    // rding = (// ) => {
+    // setRecording(true);
+    // setTimeout(() => {
+    //     setMicLoading(false);
+    // }, 1000);
+    // });
 
     const stopRecording = () => {
-        setRecording(false);
+        if (recorder) {
+            // Stop recording
+            recorder.stop();
+            setRecording(false);
+            console.log(recordedSound);
+
+            // Remove “recording” icon from browser tab
+            recorder.stream.getTracks().forEach((i) => i.stop());
+        }
     };
 
     return (
         <div className="reactMicComponent">
-            <ReactMic
-                record={recording}
-                className="sound-wave"
-                onStop={onStop}
-                // onData={onData}
-                strokeColor="#FFFFFF"
-                backgroundColor="#fe734c"
-                onBlock={startRecording}
-                mimeType="audio/mp3" // Change type wanted here
-            />
             {recordedSound && !recording && <AudioPlayer audio={recordedSound} />}
             {recordedSound && recording && <p>réenregistrement en cours</p>}
             {!recordedSound && !recording && <p>Aucun audio</p>}
 
             <div className="recording-microphone">
                 {recording && (
-                    <Icon
-                        onClick={stopRecording}
-                        name={micLoading ? "wait" : "stop circle"}
-                        size="big"
-                    />
+                    <Icon onClick={stopRecording} name={"stop circle"} size="big" />
                 )}
                 {!recording && (
                     <Icon onClick={startRecording} name="microphone" size="big" />
@@ -57,3 +73,22 @@ const ReactMicComponent = ({ setRecordedSound, recordedSound }) => {
 };
 
 export default ReactMicComponent;
+
+/* 
+
+
+
+<ReactMic
+                record={recording}
+                className="sound-wave"
+                onStop={onStop}
+                // onData={onData}
+                strokeColor="#FFFFFF"
+                backgroundColor="#fe734c"
+                onBlock={startRecording}
+                mimeType="audio/mpeg" // Change type wanted here
+/>
+
+
+*/
+// name={micLoading ? "wait" : "stop circle"}

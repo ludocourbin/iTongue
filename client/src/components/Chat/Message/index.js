164e42f60181227e335f6c568f86436ca178cdd3
GautierCo <gautier.colasse@gmail.com>
2020-09-02 01:45:32 +0200

    Chat: Design of conversation

--- client/src/components/Chat/Message/index.js
+++ client/src/components/Chat/Message/index.js
@@ -13,7 +13,7 @@ const Message = () => {
                     <Icon name="chevron circle left" size="small" />
                     Retour aux messages
                 </Link>
-                <Link to='/user/' className="message_link__user">
+                <Link to={`/user/$`} className="message_link__user">
                     <div>Ludovic</div>
                     <Image  className="message_avatar__user" src="https://itongue.s3.eu-west-3.amazonaws.com/uploads/avatars/5/6/5/7/aa0d3e447aac8a32fb8c3ae0a52c?v=1599001023114"/>
                     
@@ -24,13 +24,87 @@ const Message = () => {
                 <div className="message-list">
                     <div className="message_container">
                         <div className="message__user">
-                            <Image  className="message-avatar" src="https://itongue.s3.eu-west-3.amazonaws.com/uploads/avatars/9/e/8/3/1f5e7a08b34c5d49a68544a78bcc?v=1598564994629"/>
-                            <div className="message-name">Gautier</div>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                            <Image  className="message-avatar" src="https://itongue.s3.eu-west-3.amazonaws.com/uploads/avatars/5/6/5/7/aa0d3e447aac8a32fb8c3ae0a52c?v=1599001023114"/>
+                            {/* <div className="message-name">Gautier</div> */}
+                        </div>
+                        <div className="message_wrapper">
+                            <div className="message-text">Lorem ipsum dolor, sit amet zeas ee consectetur adipisicing elit.</div>
+                            <div className="message-date">a few seconds ago</div>
                         </div>
                     </div>
+                    <div className="message_container">
+                        <div className="message__user--right">
+                            <Image  className="message-avatar" src="https://itongue.s3.eu-west-3.amazonaws.com/uploads/avatars/9/e/8/3/1f5e7a08b34c5d49a68544a78bcc?v=1598564994629"/>
+                            {/* <div className="message-name">Gautier</div> */}
+                        </div>
+                        <div className="message_wrapper--right">
+                            <div className="message-text--right">Lorem ipsum dolor, sit amet zeas ee consectetur adipisicing elit.</div>
+                            <div className="message-date--right">a few seconds ago</div>
+                        </div>
+                    </div>
+                    <div className="message_container">
+                        <div className="message__user">
+                            <Image  className="message-avatar" src="https://itongue.s3.eu-west-3.amazonaws.com/uploads/avatars/5/6/5/7/aa0d3e447aac8a32fb8c3ae0a52c?v=1599001023114"/>
+                            {/* <div className="message-name">Gautier</div> */}
+                        </div>
+                        <div className="message_wrapper">
+                            <div className="message-text">Lorem ipsum dolor, sit amet zeas ee consectetur adipisicing elit.</div>
+                            <div className="message-date">a few seconds ago</div>
+                        </div>
+                    </div>
+                    <div className="message_container">
+                        <div className="message__user--right">
+                            <Image  className="message-avatar" src="https://itongue.s3.eu-west-3.amazonaws.com/uploads/avatars/9/e/8/3/1f5e7a08b34c5d49a68544a78bcc?v=1598564994629"/>
+                            {/* <div className="message-name">Gautier</div> */}
+                        </div>
+                        <div className="message_wrapper--right">
+                            <div className="message-text--right">Lorem ipsum dolor, sit amet zeas ee consectetur adipisicing elit.</div>
+                            <div className="message-date--right">a few seconds ago</div>
+                        </div>
+                    </div>
+                    <div className="message_container">
+                        <div className="message__user">
+                            <Image  className="message-avatar" src="https://itongue.s3.eu-west-3.amazonaws.com/uploads/avatars/5/6/5/7/aa0d3e447aac8a32fb8c3ae0a52c?v=1599001023114"/>
+                            {/* <div className="message-name">Gautier</div> */}
+                        </div>
+                        <div className="message_wrapper">
+                            <div className="message-text">Lorem ipsum dolor, sit amet zeas ee consectetur adipisicing elit.</div>
+                            <div className="message-date">a few seconds ago</div>
+                        </div>
+                    </div>
+                    <div className="message_container">
+                        <div className="message__user--right">
+                            <Image  className="message-avatar" src="https://itongue.s3.eu-west-3.amazonaws.com/uploads/avatars/9/e/8/3/1f5e7a08b34c5d49a68544a78bcc?v=1598564994629"/>
+                            {/* <div className="message-name">Gautier</div> */}
+                        </div>
+                        <div className="message_wrapper--right">
+                            <div className="message-text--right">Lorem ipsum dolor, sit amet zeas ee consectetur adipisicing elit.</div>
+                            <div className="message-date--right">a few seconds ago</div>
+                        </div>
+                    </div>
+                    <div className="message_container">
+                        <div className="message__user">
+                            <Image  className="message-avatar" src="https://itongue.s3.eu-west-3.amazonaws.com/uploads/avatars/5/6/5/7/aa0d3e447aac8a32fb8c3ae0a52c?v=1599001023114"/>
+                            {/* <div className="message-name">Gautier</div> */}
+                        </div>
+                        <div className="message_wrapper">
+                            <div className="message-text">Lorem ipsum dolor, sit amet zeas ee consectetur adipisicing elit.</div>
+                            <div className="message-date">a few seconds ago</div>
+                        </div>
+                    </div>
+                    <div className="message_container">
+                        <div className="message__user--right">
+                            <Image  className="message-avatar" src="https://itongue.s3.eu-west-3.amazonaws.com/uploads/avatars/9/e/8/3/1f5e7a08b34c5d49a68544a78bcc?v=1598564994629"/>
+                            {/* <div className="message-name">Gautier</div> */}
+                        </div>
+                        <div className="message_wrapper--right">
+                            <div className="message-text--right">Lorem ipsum dolor, sit amet zeas ee consectetur adipisicing elit.</div>
+                            <div className="message-date--right">a few seconds ago</div>
+                        </div>
+                    </div>
+
                 </div>
-                <Input icon="send" fluid className="send-message"/>
+                <Input icon="send" fluid className="send-message" placeholder="Type your message here"/>
             </div>
         </Layout>
     );

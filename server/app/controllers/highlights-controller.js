728e194b8b03f14a4dcaae4ede11d59a5bb92269
Quentin <q.lemogodeuc@gmail.com>
2020-09-02 10:23:15 +0200

    fix(controller): highlights

--- server/app/controllers/highlights-controller.js
+++ server/app/controllers/highlights-controller.js
@@ -4,7 +4,7 @@ module.exports = {
   getBestUsers: async (req, res, next) => {
     const limit = parseInt(req.params.limit, 10);
     try {
-      const bestUsers = await highlightsDatamapper.getBestUsers(query, [limit]);
+      const bestUsers = await highlightsDatamapper.getBestUsers(limit);
       res.json({ data: bestUsers });
     } catch (err) {
       next(err);
@@ -14,7 +14,7 @@ module.exports = {
   getBestTranslations: async (req, res, next) => {
     const limit = parseInt(req.params.limit, 10);
     try {
-      const bestTranslations = await highlightsDatamapper.getBestTranslations(query, [limit]);
+      const bestTranslations = await highlightsDatamapper.getBestTranslations(limit);
       res.json({ data: bestTranslations });
     } catch (err) {
       next(err);
